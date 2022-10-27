f7bdec93e993ae9c77d1ec4af2a69f362d7e1e8e
Touma <1213269491@qq.com>
2022-10-27 17:48:28 +0800

    Setup Application and Entry Point 2022/10/27

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//For use by Hazel APP
+
+#include "Hazel/Core.h"
+#include<stdio.h>
+#include "Hazel/Application.h"
+
+//------Entry Point------------
+#include "Hazel/EntryPoint.h"
+//--------------------------
